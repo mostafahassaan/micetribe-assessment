985a5ead465cfa6543640d31f7ed13d679827d33
Mostafa Hassaan <mostafa.a.hassaan@gmail.com>
2024-10-26 14:40:22 +0300

    fixed minor bug in spec file

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,6 @@ describe('AppComponent', () => {
   it(`should have the 'micetribe' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('micetribe');
   });
 
   it('should render title', () => {
